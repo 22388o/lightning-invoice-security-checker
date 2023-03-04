5843db93211d4305a4c3a1a93d5c16c3563ed59b
Amar Muratovic <discommander@protonmail.ch>
2023-03-04 16:21:47 +0100

    #01

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # lightning-invoice-security-checker
+
+This is a Python script that allows a user to input a Lightning invoice, generate a QR code for it, and then scan the QR code to decode the invoice. It also includes a function to check the validity of the invoice, using the pyln client library to interact with a Lightning node's RPC server.
+
+The script first prompts the user to input the path to their lightning-rpc file. If the file path is not valid or the script is unable to connect to the RPC server, it will exit with an error message.
+
+The check_invoice function takes in a decoded Lightning invoice and performs several checks to ensure that it is valid. These checks include verifying the payment amount, description, payment hash, payee node ID, payment preimage, and expiration time. If any of these checks fail, the function returns an error message. Additionally, if the invoice covers another invoice, the function will return an error message. If all checks pass, the function returns a message indicating that the invoice is valid.
+
+The script then prompts the user to input the Lightning invoice they want to generate a QR code for. It uses the qrcode library to generate a QR code for the invoice and displays the resulting image. The script then prompts the user to input the path to an image file containing a QR code. It uses the zbarlight library to scan the QR code and decode the invoice.
+
+Finally, the script calls the check_invoice function on the decoded invoice and prints the resulting message indicating whether the invoice is valid or not.
+
+To use this Lightning invoice QR code scanner and validator script, you will need to have Python installed on your computer. You will also need to have the following Python libraries installed:
+
+* zbarlight
+* qrcode
+* Pillow
+* pyln.client
+* requests
  
+ To install these libraries, you can use pip, which is a package manager for Python. Open a command prompt or terminal and enter the following command:
+
+    pip install zbarlight qrcode Pillow pyln-client requests
+
+Once you have the necessary libraries installed, you can run the script by opening a command prompt or terminal and navigating to the directory where the script is saved. Then enter the following command:
+
+    python lightning_invoice_qr_code.py
+
+This will run the script and prompt you for the necessary inputs, including the path to your lightning-rpc file, the Lightning invoice you want to generate a QR code for, and the path to the image file containing the QR code.
+
+The check_invoice function included in the script performs several checks to ensure that the Lightning invoice is valid. You can modify this function to add additional checks as needed.
+
+Note: This script is designed to work with a Lightning node's RPC server. You will need to have access to an active Lightning node in order to use this script.
+
---
+++ lightning_invoice_qr_code.py
@@ -0,0 +1,97 @@
+import io
+import zbarlight
+import qrcode
+import os
+from PIL import Image
+from pyln.client import LightningRpc
+import hashlib
+import requests
+
+# prompt user for RPC connection details
+rpc_path = input("Enter path to your lightning-rpc: ")
+if not os.path.isfile(rpc_path):
+    print("Error: File path is not valid")
+    exit()
+
+try:
+    rpc = LightningRpc(rpc_path)
+except:
+    print("Error: Unable to connect to RPC server")
+    exit()
+
+
+def check_invoice(invoice):
+    # get payment details from the invoice
+    amount = invoice['msatoshi'] / 1000
+    description = invoice['description']
+    payment_hash = invoice['payment_hash']
+    payee_node_id = invoice['payee_node_id']
+
+    # check that the payment amount and description are not malicious
+    if amount <= 0:
+        return "Invalid payment amount"
+    if "hack" in description.lower() or "malicious" in description.lower():
+        return "Malicious description"
+
+    # check that the payment hash matches the invoice
+    decoded = rpc.decodepay(invoice['bolt11'])
+    if decoded['payment_hash'] != payment_hash:
+        return "Invalid payment hash"
+
+    # check that the payee's node ID is valid
+    try:
+        r = requests.get("https://1ml.com/node/" + payee_node_id)
+        if r.status_code != 200:
+            return "Invalid payee node ID"
+    except:
+        return "Error checking payee node ID"
+
+    # check that the invoice has not already been paid
+    payment_status = rpc.listinvoices(payment_hash)['invoices'][0]['status']
+    if payment_status != 'unpaid':
+        return "Invoice has already been paid"
+
+    # check that the invoice has not expired
+    expiry = decoded['expiry']
+    if expiry <= 0:
+        return "Invoice has expired"
+
+    # check that the payment preimage is valid
+    payment_preimage = decoded['payment_preimage']
+    if not payment_preimage:
+        return "Invalid payment preimage"
+
+    # check for any triggers after loading the invoice
+    if decoded['description_hash'] is not None:
+        # check if there is another invoice covered by this one
+        covered_invoice_hash = hashlib.sha256(
+            decoded['description_hash'].encode()).hexdigest()
+        covered_invoice = rpc.listinvoices(covered_invoice_hash)
+        if len(covered_invoice['invoices']) > 0:
+            return "Invoice covers another invoice"
+
+    # invoice is valid
+    return "Invoice is valid"
+
+
+# generate QR code for Lightning invoice
+invoice = input("Enter Lightning invoice: ")
+qr = qrcode.QRCode(
+    version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
+qr.add_data(invoice)
+qr.make(fit=True)
+img = qr.make_image(fill_color="black", back_color="white")
+img.show()
+
+# decode QR code to get Lightning invoice
+img_path = input("Enter path to image file containing QR code: ")
+with open(img_path, 'rb') as image_file:
+    image = Image.open(io.BytesIO(image_file.read()))
+    image.load()
+
+codes = zbarlight.scan_codes('qrcode', image)
+invoice = codes[0].decode('utf-8')
+
+# check validity of invoice
+result = check_invoice(rpc.decodepay(invoice))
+print(result)
